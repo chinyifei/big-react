d014398e409db2295563344f864eb4bf301702f8
qinyifei <qinyifeivip@@163e.com>
2023-12-04 22:20:39 +0800

    feat: 调试jsx

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -55,4 +55,27 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 	return ReactElement(type, key, ref, props);
 };
 
-export const jsxDEV = jsx;
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+	for (key in config) {
+		const val = config[key];
+		if (val !== undefined) {
+			if (val === 'key') {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (val !== undefined) {
+			if (val === 'ref') {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, key)) {
+			props[key] = val;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
